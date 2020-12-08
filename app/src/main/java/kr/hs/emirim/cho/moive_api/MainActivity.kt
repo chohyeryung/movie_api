b4c5f3e9dc2f8521e2d21a0a12e206cd34e1d4e6
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-12-08 19:50:49 +0900

    text view에 정보 띄우기 commit hr2

--- app/src/main/java/kr/hs/emirim/cho/moive_api/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/cho/moive_api/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.hs.emirim.cho.moive_api
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
@@ -33,6 +34,23 @@ class MainActivity : AppCompatActivity() {
                 if(str!=null)
                     buf.append(str)
             }while(str!=null)
+
+            var rootJsonObject=JSONObject(buf.toString())
+            var level1JsonObject:JSONObject=rootJsonObject.getJSONObject("boxOfficeResult")
+            //화면에 출력하는 부분
+            runOnUiThread{
+                textView.append("result: ${level1JsonObject}\n")
+                var movieList=level1JsonObject.getJSONArray("dailyBoxOfficeList")
+
+                for(i in 0 until movieList.length()){
+                    var obj=movieList.getJSONObject(i)
+                    var rank= obj.getString("rank")
+                    var title=obj.getString("movieNm")
+                    textView.append("순위 : "+rank)
+                    textView.append("영화 제목 : "+title)
+                }
+
+            }
         }
     }
 
